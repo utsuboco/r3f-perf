9e0109ebf5c86b4ddb22e2015cf62590de21de55
onirenaud <renaud.rohlinger@gmail.com>
2021-01-02 14:19:00 +0900

    fix jest

--- test/perf.test.tsx
+++ test/perf.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Default as Perf } from '../stories/Perf.stories';
+import { Canvas } from 'react-three-fiber'
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Perf />, div);
+    ReactDOM.render(<Canvas><Perf /></Canvas>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
