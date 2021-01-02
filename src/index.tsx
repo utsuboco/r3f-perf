141b058d4a7604765fa960dd7de1c0ce4e699638
onirenaud <renaud.rohlinger@gmail.com>
2021-01-02 18:43:40 +0900

    fix SSR

--- src/index.tsx
+++ src/index.tsx
@@ -140,10 +140,8 @@ const PerfThree = () => {
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {}
 
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
 /**
- * A custom Thing component. Neat!
+ * Performance profiler component
  */
 export const Perf: FC<Props> = () => {
   const { gl } = useThree();
@@ -214,17 +212,3 @@ export const Perf: FC<Props> = () => {
     </Html>
   );
 };
-
-// export interface Props extends HTMLAttributes<HTMLDivElement> {
-//   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-//   children?: ReactChild;
-// }
-
-// // Please do not use types off of a default export module or else Storybook Docs will suffer.
-// // see: https://github.com/storybookjs/storybook/issues/9556
-// /**
-//  * A custom Thing component. Neat!
-//  */
-// export const Thing: FC<Props> = ({ children }) => {
-//   return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-// };
