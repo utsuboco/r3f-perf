47ce0cc55632f52efd7595d92f951e1f74663272
onirenaud <renaud.rohlinger@gmail.com>
2021-01-02 14:11:11 +0900

    refactor to perf

--- stories/Perf.stories.tsx
+++ stories/Perf.stories.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Bench, Props } from '../src';
+import { Perf, Props } from '../src';
 
 const meta: Meta = {
   title: 'Welcome',
-  component: Bench,
+  component: Perf,
   argTypes: {
     children: {
-      control: {
-        type: 'text',
-      },
     },
   },
   parameters: {
-    controls: { expanded: true },
+    // controls: { expanded: true },
   },
 };
 
 export default meta;
 
-const Template: Story<Props> = args => <Bench {...args} />;
+const Template: Story<Props> = args => <Perf {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
